9e163dd1cfb28e7c8bcabbcf8c45124cd5438089
labrac <raquelortegalabrac@hotmail.com>
2023-05-24 14:21:04 +0200

    Add controls.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Header } from "./components/header";
+import { Controls } from "./components/controls";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <Controls></Controls>
+    </>
+  );
 }
 
 export default App;
